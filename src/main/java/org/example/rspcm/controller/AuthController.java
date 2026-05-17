cd364a18bdecf1a4cf66a501e8c52cb1339ae6a8
endragon <asror0812@gmail.com>
2026-05-17 20:24:45 +0500

    fix

--- src/main/java/org/example/rspcm/controller/AuthController.java
+++ src/main/java/org/example/rspcm/controller/AuthController.java
@@ -22,7 +22,6 @@ public class AuthController {
 
     @PostMapping("/verify-otp")
     public ResponseEntity<Map<String, String>> verifyOtp(@Valid @RequestBody VerifyOtpRequest request) {
-
         return ResponseEntity.ok(Map.of("message", authService.verifyOtp(request)));
     }
 
